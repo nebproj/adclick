5718663a0f0f6ef9597c6d8709c816e9180d4736
sol wu <apiswswsw@gmail.com>
2018-06-01 00:08:16 -0700

    add deposit(), withdraw(), owner, pause(), unpause()

--- contract.js
+++ contract.js
@@ -1,8 +1,17 @@
 class AdClick {
   constructor() {
+    LocalContractStorage.defineProperties(this, {
+      owner: null,
+      paused: null,  // if paused, cannot retrieve fund
+      total: null,
+    });
   }
 
   init() {
+    let from = Blockchain.transaction.from;
+    this.owner = from;
+    this.paused = false;
+    this.total = new BigNumber(0);
   }
 
   /**
@@ -33,8 +42,67 @@ class AdClick {
     return "clickcount" + id;
   }
 
+  /** allow deposit fund to this contract */
+  deposit() {
+    let from = Blockchain.transaction.from;
+    let value = Blockchain.transaction.value;
+    this.total = value.plus(this.total);
+    return this.total;
+  }
+
+  /** contract owner can withdraw fund.  if it is not paused right now.  */
+  withdraw(amount) {
+    let from = Blockchain.transaction.from;
+    if (from == this.owner
+      && !this.paused
+      && amount < this.total) {
+        var result = Blockchain.transfer(from, amount);
+        if (!result) {
+          throw new Error("transfer failed.");
+        }
+        Event.Trigger("withdraw", {
+          Transfer: {
+            from: Blockchain.transaction.to,
+            to: from,
+            value: amount.toString()
+          }
+        });
+
+        this.total = new BigNumber(this.total);
+        this.total = this.total.sub(amount);
+
+        return this.total;
+    }
+  }
+
+  pause() {
+    let from = Blockchain.transaction.from;
+    if (from == this.owner) {
+      this.paused = true;
+    }
+  }
+
+  unpause() {
+    let from = Blockchain.transaction.from;
+    if (from == this.owner) {
+      this.paused = false;
+    }
+  }
+
   getClickCount(id) {
     return LocalContractStorage.get(this.storageKeywordClickCount(id));
+  }
+
+  getOwner() {
+    return this.owner;
+  }
+
+  getTotal() {
+    return this.total;
+  }
+
+  getPaused() {
+    return this.paused;
   }
 
 }
